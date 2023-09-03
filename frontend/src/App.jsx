5e7191c295ad561f14ccd50c334f5dd4b66faef3
Paul Thomas <paulthomas2107@gmail.com>
2023-09-03 10:45:45 +0100

    More

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBooks from './pages/DeleteBook';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={ } />
-      <Route path='' element={ } />
-      <Route path='' element={ } />
-      <Route path='' element={ } />
-      <Route path='' element={ } />
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/detail/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBooks />} />
     </Routes>
-    
-  )
+  );
 };
 
 export default App;
